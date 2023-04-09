b4cfa5f5573c5cb5d00016679d699cdf2f4adbc7
Jrdiver <jrdiver@users.noreply.github.com>
2023-04-08 23:05:31 -0500

    Add project files.

---
+++ DestopApp/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Microsoft.AspNetCore.Components.WebView;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DestopApp;
+
+/// <summary> Interaction logic for MainWindow.xaml/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddWpfBlazorWebView();
+        Resources.Add("services", serviceCollection.BuildServiceProvider());
+    }
+
+    private void Handle_UrlLoading(object sender, UrlLoadingEventArgs urlLoadingEventArgs)
+    {
+        if (urlLoadingEventArgs.Url.Host != "0.0.0.0")
+            urlLoadingEventArgs.UrlLoadingStrategy = UrlLoadingStrategy.OpenInWebView;
+    } 
+}
