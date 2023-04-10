cfbaa3b439c99b605928f1611dfd7424e3de706e
Jrdiver <jrdiver@users.noreply.github.com>
2023-04-10 12:01:20 -0500

    update

--- DestopApp/Data/WeatherForecast.cs
+++ DestopApp/Data/WeatherForecast.cs
@@ -8,5 +8,29 @@ public class WeatherForecast
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string Summary { get; set; }
+    public string Summary
+    {
+        get
+        {
+            if (TemperatureF < 10)
+                return "Freezing";
+            if (TemperatureF < 32)
+                return "Bracing";
+            if (TemperatureF < 45)
+                return "Chilly";
+            if (TemperatureF < 65)
+                return "Cool";
+            if (TemperatureF < 80)
+                return "Mild";
+            if (TemperatureF < 90)
+                return "Warm";
+            if (TemperatureF < 95)
+                return "Balmy";
+            if (TemperatureF < 100)
+                return "Hot";
+            if (TemperatureF < 105)
+                return "Sweltering";
+            return "Scorching";
+        }
+    }
 }
